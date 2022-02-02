74e177ef9ce3c4b4c6f65fc8eb5a7f7d1f3d63b3
Vinicius <viniciuskmv@gmail.com>
2022-02-02 14:37:39 -0300

    criacao componente de lista de filmes

---
+++ src/components/MovieList/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Movie from '../Movie';
+import * as S from './styles';
+
+const MovieList = () => {
+  return (
+    <S.Container>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+    </S.Container>
+  );
+};
+
+export default MovieList;
---
+++ src/components/MovieList/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const Container = styled.div`
+  display: inline-flex;
+  flex-wrap: wrap;
+  margin: 29px 112px;
+  gap: 32px;
+
+  ${media.lessThan('medium')`
+    margin: 29px 16px;
+    gap: 16px;
+    justify-content:flex-start ;
+`}
+`;
