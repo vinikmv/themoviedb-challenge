773445060a428caa4f9721127edce23bb49ab224
Vinicius <viniciuskmv@gmail.com>
2022-02-02 11:35:50 -0300

    adicionar estilos responsivos para o GenreBox

--- src/components/GenreBox/styles.js
+++ src/components/GenreBox/styles.js
@@ -1,30 +1,63 @@
-import styled from 'styled-components'
-import media from 'styled-media-query'
+import styled from 'styled-components';
+import media from 'styled-media-query';
 
 export const Container = styled.section`
-display: flex;
-width: 100%;
-height: 449px;
-background-color: #2D0C5E;
-align-items: center;
-justify-content: center;
-flex-direction: column;
+  display: flex;
+  background-color: #2d0c5e;
+  flex-direction: column;
+  width: 100%;
+  height: 449px;
+
+  ${media.greaterThan('medium')`
+    align-items: center;
+    justify-content: center;
+  `}
+
+  ${media.lessThan('medium')`
+  height: 100vh;
+  width: 100vh;
+`}
+`;
+
+export const GenreContainer = styled.div`
+
 `
 
 export const Title = styled.h1`
-color: #FFFFFF;
-font-size: 48px;
-line-height: 56px;
-width: 781px;
-text-align: center;
-padding-bottom: 40px;
-`
+  color: #ffffff;
+  font-size: 48px;
+  line-height: 56px;
+  width: 781px;
+  text-align: center;
+  padding-bottom: 40px;
+
+  ${media.lessThan('medium')`
+    font-size: 24px;
+    line-height: 28px;
+    text-align: left;
+    width: 40vh;
+    padding-top: 40px;
+    padding-left: 16px
+  `}
+`;
 
 export const FilterText = styled.h4`
-color: #FFFFFF;
-font-size: 14px;
-padding-bottom: 16px;
-text-transform: uppercase;
-`
+  color: #ffffff;
+  font-size: 14px;
+  padding-bottom: 16px;
+  text-transform: uppercase;
 
-export const Genres = styled.div``
+  ${media.lessThan('medium')`
+    text-align: left;
+    width: 40vh;
+    padding-left: 16px
+  `}
+`;
+
+export const Genres = styled.div`
+  ${media.lessThan('medium')`
+    text-align: left;
+    width: 40vh;
+    padding-left: 16px
+  `}
+`;
