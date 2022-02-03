412a1187b33158ea68698c09ca1573be85aa6f28
Vinicius <viniciuskmv@gmail.com>
2022-02-03 10:24:49 -0300

    adicionar listagem de filmes populares da api

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../Card';
 import * as S from './styles';
+import tmdbApi from '../../api/tmdbApi';
+import apiConfig from '../../api/config';
 
 const MovieList = () => {
+  const [itens, setItens] = useState([]);
+
+  const formatDate = (data) => {
+    const newDate = new Date(data)
+    const month = newDate.toLocaleString('pt-br', {month: 'short'}).toUpperCase().replace('.', '')
+    return `${newDate.getDay()} ${month} ${newDate.getFullYear()} `
+  }
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      let response = await tmdbApi.getPopularMovies();
+      setItens(response.results);
+    };
+    getMovieList();
+  }, []);
   return (
     <S.Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {!!itens &&
+        itens.map((item) => {
+          return (
+            <Card
+              key={item.id}
+              src={apiConfig.imagemOriginal(item.poster_path)}
+              title={item.title}
+              subtitle={formatDate(item.release_date)}
+            />
+          );
+        })}
     </S.Container>
   );
 };
