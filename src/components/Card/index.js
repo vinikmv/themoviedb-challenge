791fdc12ac11991c63a0dc013433d8023a5a91fe
Vinicius <viniciuskmv@gmail.com>
2022-02-03 10:23:48 -0300

    ajuste no Card para receber os parametros

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as S from './styles';
 
-const Card = ({src, title, subtitle}) => {
+const Card = ({ src, title, subtitle }) => {
   return (
     <S.Container>
-    <S.Image src='https://www.thedb.org/t/p/w440_and_h660_face/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg'></S.Image>
-    <S.Title>Homem-Aranha: Sem Volta Para Casa</S.Title>
-    <S.Subtitle>15 de dez de 2021</S.Subtitle>
+      <S.Image src={src}></S.Image>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
     </S.Container>
   );
 };
