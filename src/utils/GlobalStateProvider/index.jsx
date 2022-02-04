bfe696d65e620f4249fdad96d4332e7facc81c6d
Vinicius <viniciuskmv@gmail.com>
2022-02-04 13:58:59 -0300

    Ajuste para utilizar o useMemo

--- src/utils/GlobalStateProvider/index.jsx
+++ src/utils/GlobalStateProvider/index.jsx
@@ -1,11 +1,11 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useMemo } from 'react';
 
 export const Context = createContext();
 
 const GlobalStateProvider = ({ children }) => {
   const genreReducer = (state, action) => {
     let genresArray = JSON.parse(localStorage.getItem('genres')) || [];
-    
+
     switch (action.type) {
       case 'addGenre': {
         if (!genresArray.some((item) => item.genre === action.payload)) {
@@ -32,11 +32,11 @@ const GlobalStateProvider = ({ children }) => {
   const initialState = JSON.parse(localStorage.getItem('genres')) || [];
   const [state, dispatch] = useReducer(genreReducer, initialState);
 
-  return (
-    <Context.Provider value={{ genreState: state, genreDispatch: dispatch }}>
-      {children}
-    </Context.Provider>
-  );
+  const contextValue = useMemo(() => {
+    return { genreState: state, genreDispatch: dispatch };
+  }, [state, dispatch]);
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 };
 
 export default GlobalStateProvider;
