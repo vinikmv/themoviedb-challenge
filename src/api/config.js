dfb93175c4ce65924e6dac3877a883919406cd18
Vinicius <viniciuskmv@gmail.com>
2022-02-05 16:55:36 -0300

    Ajuste para utilizar variaveis de ambiente

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const apiConfig = {
   baseUrl: 'https://api.themoviedb.org/3/',
-  apiKey: '77b1281113894ee7988fdf25bbbd3b1b',
+  apiKey: API_KEY,
   imagemOriginal: (path) => `https://image.tmdb.org/t/p/original/${path}`,
   imagemW500: (path) => `https://image.tmdb.org/t/p/w500/${path}`,
 };
